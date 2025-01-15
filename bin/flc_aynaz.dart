5e69a235020f1ecdb88ead87fb6fbbe4d2a0bf4d
aynaz2002 <stay2002skz@gmail.com>
2025-01-15 19:13:54 +0330

    initial commit

---
+++ bin/flc_aynaz.dart
@@ -0,0 +1,5 @@
+import 'package:flc_aynaz/flc_aynaz.dart' as flc_aynaz;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flc_aynaz.calculate()}!');
+}
---
+++ test/flc_aynaz_test.dart
@@ -0,0 +1,8 @@
+import 'package:flc_aynaz/flc_aynaz.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
